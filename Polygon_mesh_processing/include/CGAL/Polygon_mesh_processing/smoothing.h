6c07ae723022d69c3499746552911406cb00868e
konstantinos katrioplas <konst.katrioplas@gmail.com>
2017-09-24 00:14:40 +0300

    coeff matrix

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/smoothing.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/smoothing.h
@@ -9,7 +9,7 @@
 
 #include <CGAL/Eigen_solver_traits.h>
 #include <CGAL/Eigen_matrix.h>
-#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 namespace CGAL {
 
@@ -32,18 +32,22 @@ struct Cotangent_weight : CotangentValue
 
     typedef typename boost::graph_traits<PolygonMesh>::halfedge_descriptor   halfedge_descriptor;
     typedef typename boost::graph_traits<PolygonMesh>::vertex_descriptor     vertex_descriptor;
-    typedef std::pair<halfedge_descriptor, halfedge_descriptor>              he_pair;
 
-    double operator()(halfedge_descriptor he, he_pair incd_edges)
+    double operator()(halfedge_descriptor he)
     {
+      halfedge_descriptor h1 = next(he, pmesh());
+      halfedge_descriptor h2 = prev(opposite(he, pmesh()), pmesh());
+
       vertex_descriptor vs = source(he, pmesh());
       vertex_descriptor vt = target(he, pmesh());
-      vertex_descriptor v1 = target(incd_edges.first, pmesh());
-      vertex_descriptor v2 = source(incd_edges.second, pmesh());
+      vertex_descriptor v1 = target(h1, pmesh());
+      vertex_descriptor v2 = source(h2, pmesh());
 
-      return ( CotangentValue::operator()(vs, v1, vt) + CotangentValue::operator()(vs, v2, vt) );
+      return ( CotangentValue::operator()(vs, v1, vt) + CotangentValue::operator()(vs, v2, vt) ) / 2.0;
     }
 };
+
+
 
 
 
@@ -53,22 +57,79 @@ struct Cotangent_weight : CotangentValue
 template<typename PolygonMesh, typename VertexPointMap>
 class Shape_smoother{
 
+private:
+
+
+    typedef typename boost::graph_traits<PolygonMesh>::vertex_descriptor vertex_descriptor;
+    typedef typename boost::graph_traits<PolygonMesh>::halfedge_descriptor halfedge_descriptor;
+
+    typedef CGAL::Eigen_solver_traits< Eigen::SimplicialLDLT< Eigen::SparseMatrix<double> > > Solver_traits;
+
+
+    // vertex index map
+    typedef typename boost::property_map<PolygonMesh, boost::vertex_index_t>::type IndexMap;
+    IndexMap idxmap = get(boost::vertex_index, mesh_);
+
+
+
+
 
 public:
-    Shape_smoother(PolygonMesh& mesh, VertexPointMap& vpmap) : mesh_(mesh), vpmap_(vpmap), weight_calculator_(mesh, vpmap) {}
 
 
-    void calculate_coefficients_matrix()
+    Shape_smoother(PolygonMesh& mesh, VertexPointMap& vpmap) : mesh_(mesh), vpmap_(vpmap), weight_calculator_(mesh, vpmap)
+    {
+        nb_vertices = vertices(mesh_).size();
+    }
+
+
+
+
+    void calculate_coeff_matrix()
     {
 
+
+        // linear system data
+        typename Solver_traits::Matrix A(nb_vertices, nb_vertices);
+
+
+
+        for(vertex_descriptor vi : vertices(mesh_))
+        {
+            if(!is_border(vi, mesh_))
+            {
+                double sum_L = 0;
+                for(halfedge_descriptor h : halfedges_around_source(v, mesh_))
+                {
+                    // calculate weight
+                    double Lij = weight_calculator_(h);
+                    sum_L += Lij;
+
+                    vertex_descriptor vj = target(h, mesh_);
+
+                    A.add_coef(idxmap[vi], idxmap[vj], Lij);
+                }
+
+                A.add_coef(idxmap[vi], idxmap[vi], sum_L);
+
+            }
+
+
+        }
+
+
+
     }
+
+
+
 
 
 
     void solve_linear_system()
     {
 
-        typedef CGAL::Eigen_solver_traits<> Eigen_solver;
+        typedef CGAL::Eigen_solver_traits<Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>> Eigen_solver;
 
 
         std::size_t degree = 2;
@@ -105,15 +166,8 @@ public:
 
         std::cout<<"X= "<<X<<std::endl;
 
-        //std::cout<<"A= "<<A[0]<<std::endl;
-
-
-
 
     }
-
-
-
 
 
 
@@ -123,12 +177,20 @@ private:
     PolygonMesh& mesh_;
     VertexPointMap& vpmap_;
     Cotangent_weight<PolygonMesh, VertexPointMap> weight_calculator_;
+    Solver_traits solver_;
+    std::size_t nb_vertices;
 
-    // linear system data
+
 
 
 
 };
+
+
+
+
+
+
 
 
 template<typename PolygonMesh>
@@ -140,7 +202,7 @@ void smooth_shape(PolygonMesh& mesh)
     VertexPointMap vpmap = get(CGAL::vertex_point, mesh);
 
     CGAL::Polygon_mesh_processing::Shape_smoother<PolygonMesh, VertexPointMap> smoother(mesh, vpmap);
-    smoother.solve_linear_system();
+    smoother.calculate_coeff_matrix();
 
 
 }
